8de4f0f4d8171e247a72d3bab0c069fcd3f3c75c
Miquel Roig <miquel.roig@urv.cat>
2020-12-24 12:53:41 +0100

    try1

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre");
+            string nom = Console.ReadLine();
+
+            Console.WriteLine("Escribe el nombre de una ciudad");
+            string ciutat = Console.ReadLine();
+
+            Console.WriteLine("Hola {0} bienvenido a {1}",nom,ciutat);
+            
+        }
+    }
+}
---
+++ Program5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp10
+{
+    class Program5
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Escribe un dia de la semana");
+                string semana = Console.ReadLine();
+
+                semana = semana.ToLowerInvariant();
+
+            List<String> lista = new List<string>();   
+            lista.Add("lunes");
+            lista.Add("martes");
+            lista.Add("miercoles");
+            lista.Add("jueves");
+            lista.Add("viernes");
+            lista.Add("sabado");
+            lista.Add("domingo");
+
+            bool has = lista.Exists(x => x == semana);
+            if (has)
+            {
+
+                if (semana.Equals("domingo") || semana.Equals("sabado"))
+                    Console.WriteLine("Es finde");
+                else
+                    Console.WriteLine("No finde :(");
+            }
+            else
+                Console.WriteLine("No es ni un nombre de la semana");
+
+        }
+    }
+}
